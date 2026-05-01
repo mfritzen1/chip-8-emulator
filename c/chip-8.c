8a77db179196c204e258744a2fbfe3526193d925
mfritzen1 <masonfritzen.dev@gmail.com>
2026-04-30 23:00:53 -0400

    moved non-instructions from instructions.c to chip-8.c

--- c/chip-8.c
+++ c/chip-8.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
 #include "instructions.h"
 
+#define SCREEN_HEIGHT 32
+#define SCREEN_WIDTH 64
+#define MEMORY_SIZE 4096
+#define FONT_START 0x50
+
 int main()
 {
-  ins00E0();
+	unsigned char memory[MEMORY_SIZE];
+	unsigned char dataRegister[16];
+	unsigned short I;
+	unsigned short pc;
+	unsigned char delayTimer;
+	unsigned char soundTimer;
+	unsigned short stack[16];
+	unsigned short sp;
 
-  return 0;
+	const unsigned short FONT_0_LOCATION = FONT_START;
+	const unsigned short FONT_1_LOCATION = FONT_START + 5;
+	const unsigned short FONT_2_LOCATION = FONT_START + 10;
+	const unsigned short FONT_3_LOCATION = FONT_START + 15;
+	const unsigned short FONT_4_LOCATION = FONT_START + 20;
+	const unsigned short FONT_5_LOCATION = FONT_START + 25;
+	const unsigned short FONT_6_LOCATION = FONT_START + 30;
+	const unsigned short FONT_7_LOCATION = FONT_START + 35;
+	const unsigned short FONT_8_LOCATION = FONT_START + 40;
+	const unsigned short FONT_9_LOCATION = FONT_START + 45;
+	const unsigned short FONT_A_LOCATION = FONT_START + 50;
+	const unsigned short FONT_B_LOCATION = FONT_START + 55;
+	const unsigned short FONT_C_LOCATION = FONT_START + 60;
+	const unsigned short FONT_D_LOCATION = FONT_START + 65;
+	const unsigned short FONT_E_LOCATION = FONT_START + 70;
+	const unsigned short FONT_F_LOCATION = FONT_START + 75;
+
+	unsigned char font[80] = {
+			0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+			0x20, 0x60, 0x20, 0x20, 0x70, // 1
+			0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+			0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+			0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+			0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+			0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+			0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+			0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+			0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+			0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+			0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+			0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+			0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+			0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+			0xF0, 0x80, 0xF0, 0x80, 0x80	// F
+	};
+
+	return 0;
 }
--- c/instructions.c
+++ c/instructions.c
@@ -1,48 +1,3 @@
-unsigned char memory[4096];
-unsigned char dataRegister[16];
-unsigned short I;
-unsigned short pc;
-unsigned char delayTimer;
-unsigned char soundTimer;
-unsigned short stack[16];
-unsigned short sp;
-
-const unsigned short FONT_0_LOCATION = 0x50;
-const unsigned short FONT_1_LOCATION = 0x55;
-const unsigned short FONT_2_LOCATION = 0x5A;
-const unsigned short FONT_3_LOCATION = 0x5F;
-const unsigned short FONT_4_LOCATION = 0x64;
-const unsigned short FONT_5_LOCATION = 0x69;
-const unsigned short FONT_6_LOCATION = 0x6E;
-const unsigned short FONT_7_LOCATION = 0x73;
-const unsigned short FONT_8_LOCATION = 0x78;
-const unsigned short FONT_9_LOCATION = 0x7D;
-const unsigned short FONT_A_LOCATION = 0x82;
-const unsigned short FONT_B_LOCATION = 0x87;
-const unsigned short FONT_C_LOCATION = 0x8C;
-const unsigned short FONT_D_LOCATION = 0x91;
-const unsigned short FONT_E_LOCATION = 0x96;
-const unsigned short FONT_F_LOCATION = 0x9B;
-
-unsigned char font[80] = {
-    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-    0x20, 0x60, 0x20, 0x20, 0x70, // 1
-    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-};
-
 /** 0NNN - execute machine code (probably won't be used) */
 void ins0NNN() {}
 
@@ -50,149 +5,149 @@ void ins0NNN() {}
 void ins00E0() {}
 
 /** 00EE - return from subroutine */
-void ins00EE()
+void ins00EE(unsigned short *pc, unsigned short *stack[], unsigned short *sp)
 {
-    pc = stack[sp];
-    sp--;
+    *pc = *stack[*sp];
+    *sp--;
 }
 
 /** 1NNN - jump to address NNN */
 // TODO: depending on implementation, might need to decrement pc after assignment here
-void ins1NNN(unsigned short address)
+void ins1NNN(unsigned short address, unsigned short *pc)
 {
-    pc = address;
+    *pc = address;
 }
 
 /** 2NNN - call subroutine at NNN */
-void ins2NNN(unsigned short address)
+void ins2NNN(unsigned short address, unsigned short *pc, unsigned short *stack[], unsigned short *sp)
 {
-    stack[sp + 1] = pc;
-    sp++;
-    pc = address;
+    *stack[*sp + 1] = *pc;
+    *sp++;
+    *pc = address;
 }
 
 /** 3XNN - skip the next instruction if VX == NN */
-void ins3XNN(unsigned char registerX, unsigned char value)
+void ins3XNN(unsigned char registerX, unsigned char value, unsigned char *dataRegister[], unsigned short *pc)
 {
-    if (dataRegister[registerX] == value)
+    if (*dataRegister[registerX] == value)
     {
-        pc += 2;
+        *pc += 2;
     }
 }
 
 /** 4XNN - skip the next instruction if VX != NN */
-void ins4XNN(unsigned char registerX, unsigned char value)
+void ins4XNN(unsigned char registerX, unsigned char value, unsigned char *dataRegister[], unsigned short *pc)
 {
-    if (dataRegister[registerX] != value)
+    if (*dataRegister[registerX] != value)
     {
-        pc += 2;
+        *pc += 2;
     }
 }
 
 /** 5XY0 - skip the next instruction if VX == VY */
-void ins5XY0(unsigned char registerX, unsigned char registerY)
+void ins5XY0(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[], unsigned short *pc)
 {
-    if (dataRegister[registerX] == dataRegister[registerY])
+    if (*dataRegister[registerX] == *dataRegister[registerY])
     {
-        pc += 2;
+        *pc += 2;
     }
 }
 
 /** 6XNN - set VX = NN */
-void ins6XNN(unsigned char registerX, unsigned char value)
+void ins6XNN(unsigned char registerX, unsigned char value, unsigned char *dataRegister[])
 {
-    dataRegister[registerX] = value;
+    *dataRegister[registerX] = value;
 }
 
 /** 7XNN - set VX += NN, don't change carry flag */
-void ins7XNN(unsigned char registerX, unsigned char value)
+void ins7XNN(unsigned char registerX, unsigned char value, unsigned char *dataRegister[])
 {
-    dataRegister[registerX] += value;
+    *dataRegister[registerX] += value;
 }
 
 /** 8XY0 - set VX = VY */
-void ins8XY0(unsigned char registerX, unsigned char registerY)
+void ins8XY0(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    dataRegister[registerX] = dataRegister[registerY];
+    *dataRegister[registerX] = *dataRegister[registerY];
 }
 
 /** 8XY1 - set VX |= VY */
-void ins8XY1(unsigned char registerX, unsigned char registerY)
+void ins8XY1(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    dataRegister[registerX] |= dataRegister[registerY];
+    *dataRegister[registerX] |= *dataRegister[registerY];
 }
 
 /** 8XY2 - set VX &= VY */
-void ins8XY2(unsigned char registerX, unsigned char registerY)
+void ins8XY2(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    dataRegister[registerX] &= dataRegister[registerY];
+    *dataRegister[registerX] &= *dataRegister[registerY];
 }
 
 /** 8XY3 - set VX ^= VY */
-void ins8XY3(unsigned char registerX, unsigned char registerY)
+void ins8XY3(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    dataRegister[registerX] ^= dataRegister[registerY];
+    *dataRegister[registerX] ^= *dataRegister[registerY];
 }
 
 /** 8XY4 - set VX += VY, set VF to 1 for overflow */
-void ins8XY4(unsigned char registerX, unsigned char registerY)
+void ins8XY4(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    int sum = dataRegister[registerX] + dataRegister[registerY];
-    dataRegister[0xf] = sum > 255 ? 0x1 : 0x0;
-    dataRegister[registerX] = sum;
+    int sum = *dataRegister[registerX] + *dataRegister[registerY];
+    *dataRegister[0xf] = sum > 255 ? 0x1 : 0x0;
+    *dataRegister[registerX] = sum;
 }
 
 /** 8XY5 - set VX -= VY, set VF to 0 for underflow */
-void ins8XY5(unsigned char registerX, unsigned char registerY)
+void ins8XY5(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    int difference = dataRegister[registerX] - dataRegister[registerY];
-    dataRegister[0xf] = dataRegister[registerX] >= dataRegister[registerY] ? 1 : 0;
-    dataRegister[registerX] = difference;
+    int difference = *dataRegister[registerX] - *dataRegister[registerY];
+    *dataRegister[0xf] = *dataRegister[registerX] >= *dataRegister[registerY] ? 1 : 0;
+    *dataRegister[registerX] = difference;
 }
 
 /** 8XY6 - set VX >>= 1 (right shift by 1), set VF to LSB of VX */
-void ins8XY6(unsigned char registerX)
+void ins8XY6(unsigned char registerX, unsigned char *dataRegister[])
 {
-    unsigned char valueX = dataRegister[registerX];
-    dataRegister[0xf] = valueX & 0x1;
-    dataRegister[registerX] = valueX >> 1;
+    unsigned char valueX = *dataRegister[registerX];
+    *dataRegister[0xf] = valueX & 0x1;
+    *dataRegister[registerX] = valueX >> 1;
 }
 
 /** 8XY7 - set VX = VY - VX, set VF to 1 if VY > VX */
-void ins8XY7(unsigned char registerX, unsigned char registerY)
+void ins8XY7(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[])
 {
-    int difference = dataRegister[registerY] - dataRegister[registerX];
-    dataRegister[0xf] = dataRegister[registerY] >= dataRegister[registerX] ? 1 : 0;
-    dataRegister[registerX] = difference;
+    int difference = *dataRegister[registerY] - *dataRegister[registerX];
+    *dataRegister[0xf] = *dataRegister[registerY] >= *dataRegister[registerX] ? 1 : 0;
+    *dataRegister[registerX] = difference;
 }
 
 /** 8XYE - set VX <<= 1 (left shift by 1), set VF to MSB of VX */
-void ins8XYE(unsigned char registerX)
+void ins8XYE(unsigned char registerX, unsigned char *dataRegister[])
 {
-    unsigned char valueX = dataRegister[registerX];
-    dataRegister[0xf] = (valueX & 0x80) >> 7;
-    dataRegister[registerX] = valueX << 1;
+    unsigned char valueX = *dataRegister[registerX];
+    *dataRegister[0xf] = (valueX & 0x80) >> 7;
+    *dataRegister[registerX] = valueX << 1;
 }
 
 /** 9XY0 - skip the next instruction if VX != VY */
-void ins9XY0(unsigned char registerX, unsigned char registerY)
+void ins9XY0(unsigned char registerX, unsigned char registerY, unsigned char *dataRegister[], unsigned short *pc)
 {
-    if (dataRegister[registerX] != dataRegister[registerY])
+    if (*dataRegister[registerX] != *dataRegister[registerY])
     {
-        pc += 2;
+        *pc += 2;
     }
 }
 
 /** ANNN - set I = NNN */
-void insANNN(unsigned short address)
+void insANNN(unsigned short address, unsigned short *I)
 {
-    I = address;
+    *I = address;
 }
 
 /** BNNN - set PC = V0 + NNN */
-void insBNNN(unsigned short address)
+void insBNNN(unsigned short address, unsigned char *dataRegister[], unsigned short *pc)
 {
-    pc = dataRegister[0x0] + address;
+    *pc = *dataRegister[0x0] + address;
 }
 
 /** CXNN - set VX = rand() & NN */
@@ -208,112 +163,112 @@ void insEX9E() {}
 void insEXA1() {}
 
 /** FX07 - set VX to value of delay timer */
-void insFX07(unsigned char registerX)
+void insFX07(unsigned char registerX, unsigned char *dataRegister[], unsigned char *delayTimer)
 {
-    dataRegister[registerX] = delayTimer;
+    *dataRegister[registerX] = *delayTimer;
 }
 
 /** FX0A - wait for key press, then store value in VX */
 void insFX0A() {}
 
 /** FX15 - set delay timer to VX */
-void insFX15(unsigned char registerX)
+void insFX15(unsigned char registerX, unsigned char *dataRegister[], unsigned char *delayTimer)
 {
-    delayTimer = dataRegister[registerX];
+    *delayTimer = *dataRegister[registerX];
 }
 
 /** FX18 - set sound timer to VX */
-void insFX18(unsigned char registerX)
+void insFX18(unsigned char registerX, unsigned char *dataRegister[], unsigned char *soundTimer)
 {
-    soundTimer = dataRegister[registerX];
+    *soundTimer = *dataRegister[registerX];
 }
 
 /** FX1E - set I += VX */
-void insFX1E(unsigned char registerX)
+void insFX1E(unsigned char registerX, unsigned char *dataRegister[], unsigned short *I)
 {
-    I += dataRegister[registerX];
+    *I += *dataRegister[registerX];
 }
 
 /** FX29 - set I to memory location of font character in VX */
-void insFX29(unsigned char registerX)
+void insFX29(unsigned char registerX, unsigned short fontStartLocation, unsigned char *dataRegister[], unsigned short *I)
 {
-    unsigned char fontCharacter = dataRegister[registerX];
+    unsigned char fontCharacter = *dataRegister[registerX];
     switch (fontCharacter)
     {
     case 0x0:
-        I = FONT_0_LOCATION;
+        *I = fontStartLocation;
         break;
     case 0x1:
-        I = FONT_1_LOCATION;
+        *I = fontStartLocation + 5;
         break;
     case 0x2:
-        I = FONT_2_LOCATION;
+        *I = fontStartLocation + 10;
         break;
     case 0x3:
-        I = FONT_3_LOCATION;
+        *I = fontStartLocation + 15;
         break;
     case 0x4:
-        I = FONT_4_LOCATION;
+        *I = fontStartLocation + 20;
         break;
     case 0x5:
-        I = FONT_5_LOCATION;
+        *I = fontStartLocation + 25;
         break;
     case 0x6:
-        I = FONT_6_LOCATION;
+        *I = fontStartLocation + 30;
         break;
     case 0x7:
-        I = FONT_7_LOCATION;
+        *I = fontStartLocation + 35;
         break;
     case 0x8:
-        I = FONT_8_LOCATION;
+        *I = fontStartLocation + 40;
         break;
     case 0x9:
-        I = FONT_9_LOCATION;
+        *I = fontStartLocation + 45;
         break;
     case 0xA:
-        I = FONT_A_LOCATION;
+        *I = fontStartLocation + 50;
         break;
     case 0xB:
-        I = FONT_B_LOCATION;
+        *I = fontStartLocation + 55;
         break;
     case 0xC:
-        I = FONT_C_LOCATION;
+        *I = fontStartLocation + 60;
         break;
     case 0xD:
-        I = FONT_D_LOCATION;
+        *I = fontStartLocation + 65;
         break;
     case 0xE:
-        I = FONT_E_LOCATION;
+        *I = fontStartLocation + 70;
         break;
     default:
-        I = FONT_F_LOCATION;
+        *I = fontStartLocation + 75;
         break;
     }
 }
 
 /** FX33 - store BCD of VX in memory locations I, I + 1, and I + 2 */
-void insFX33(unsigned char registerX)
+void insFX33(unsigned char registerX, unsigned char *dataRegister[], unsigned short *I, unsigned char *memory[])
 {
-    unsigned char dataX = dataRegister[registerX];
-    memory[I] = dataX / 100;
-    memory[I + 1] = (dataX % 100) / 10;
-    memory[I + 2] = dataX % 10;
+    unsigned char dataX = *dataRegister[registerX];
+    *memory[*I] = dataX / 100;
+    *memory[*I + 1] = (dataX % 100) / 10;
+    *memory[*I + 2] = dataX % 10;
 }
 
 /** FX55 - stores V0 through VX in memory, starting at location I */
-void insFX55(unsigned char registerX)
+void insFX55(unsigned char registerX, unsigned char *dataRegister[], unsigned short *I, unsigned char *memory[])
 {
     for (unsigned char i = 0x0; i <= registerX; i++)
     {
-        memory[I + i] = dataRegister[i];
+        *memory[*I + i] = *dataRegister[i];
     }
 }
 
 /** FX65 - fills V0 through VX with data from memory, starting at location I */
-void insFX65(unsigned char registerX)
+void insFX65(unsigned char registerX, unsigned char *dataRegister[], unsigned short *I, unsigned char *memory[])
 {
     for (unsigned char i = 0x0; i <= registerX; i++)
     {
-        dataRegister[i] = memory[I + i];
+        *dataRegister[i] = *memory[*I + i];
     }
 }
